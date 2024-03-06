b13d51c2677280e0c1084f3dc071de9297ba5e98
shailajanimmagari <160123202+shailajanimmagari@users.noreply.github.com>
2024-03-06 13:01:59 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,7 @@
+function myfun(){
+    document.getElementById("para").innerHTML="Here you will download your tickets for the concert";
+}
+function myFun1(){
+    document.getElementById("parag").innerHTML="Bts is kpop boy band from BigHit Entertainment. Bts means Bulletproof Boyscoucts in korean but they have recently changes their english name to Beyond the Scene. They have seven members (3 rappers and 4 vocalists) and debuted on June 12th, 2013. Their fandom name is Army. And The BTS band’s name is famous in the music world, and it is always fun to watch their performance. BTS has announced its world tour recently. BTS may also declare its India Tour 2024 soon. They are gsoing to perfom wrldwide with a Tour name Reunion Tour in 2025. So as we all know that BTS is a famous Korean band. Due to their music and performances, BTS is very popular. They will organize their concert in India soon. At present, no official dates are declared for it.";
+    
+}
